fcc0f5e6cb99679d29de86a4b091369feb825b00
Justin Hollmer <justin.hollmer@gmail.com>
2022-11-02 13:35:21 -0500

    off to a good start with the order controller

--- src/main/java/com/ecommersebackend/ecommercebackend/Controllers/OrderController.java
+++ src/main/java/com/ecommersebackend/ecommercebackend/Controllers/OrderController.java
@@ -1,9 +1,14 @@
 package com.ecommersebackend.ecommercebackend.Controllers;
 
+import com.ecommersebackend.ecommercebackend.Models.Order;
 import com.ecommersebackend.ecommercebackend.Repos.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -11,6 +16,15 @@ public class OrderController {
     @Autowired
     OrderRepo orderRepo;
 
-//    @GetMapping(value = "/get-order-user/{}")
+    @GetMapping(value = "/get-orders")
+    public List<Order> getOrders() {
+        return orderRepo.findAll();
+    }
+
+    @PostMapping(value = "/post-order")
+    public String postOrder(@RequestBody Order order) {
+        orderRepo.save(order);
+        return "IT WORKED";
+    }
 
 }
