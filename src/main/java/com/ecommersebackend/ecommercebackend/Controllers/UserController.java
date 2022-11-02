62ea6b0838f96e9a73a5e524d3a215f72b5308a9
Justin Hollmer <justin.hollmer@gmail.com>
2022-11-02 11:05:04 -0500

    added put functionality to the User model

---
+++ src/main/java/com/ecommersebackend/ecommercebackend/Controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.ecommersebackend.ecommercebackend.Controllers;
+
+import com.ecommersebackend.ecommercebackend.Models.User;
+import com.ecommersebackend.ecommercebackend.Repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepo userRepo;
+
+//    Default
+    @GetMapping(value = "/")
+    public String getPage() {
+        return "HELLO THERE";
+    }
+
+//    Get
+    @GetMapping(value = "/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+//    Post
+    @PostMapping(value = "/save-user")
+    public String postUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "IT WORKS LETS GOOOOOOOOOOOOOO";
+    }
+
+//    Update
+    @PutMapping(value = "/update-user/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setUsername(user.getUsername());
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setPassword(user.getPassword());
+        updatedUser.setAdmin_status(user.getAdmin_status());
+        userRepo.save(updatedUser);
+        return "Updated";
+    }
+
+//    Delete
+
+}
